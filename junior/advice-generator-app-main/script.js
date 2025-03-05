d3063aeeaed1aae1eb32149a4bf56c0e954ce494
Asia Ashraf <asiaashraf7272@gmail.com>
2025-03-05 17:51:36 +0500

    new challange added

--- junior/advice-generator-app-main/script.js
+++ junior/advice-generator-app-main/script.js
@@ -2,14 +2,14 @@ let getAdviceBtn = document.getElementById("advice-icon-btn");
 let quoteBox = document.getElementById("advice-quote")
 let AdviceIdBox = document.getElementById("advice-id")
 
-// let getAdvice = ()=>{
-//     fetch("	https://api.adviceslip.com/advice")
-//     .then((res)=> res.json())
-//     .then((data)=>{
-//         console.log(data.slip.id)
-//         quoteBox.innerText = `"${data.slip.advice}"`
-//         AdviceIdBox.innerHTML = `${data.slip.id}`
-//     })
-// }
+let getAdvice = ()=>{
+    fetch("	https://api.adviceslip.com/advice")
+    .then((res)=> res.json())
+    .then((data)=>{
+        console.log(data.slip.id)
+        quoteBox.innerText = `"${data.slip.advice}"`
+        AdviceIdBox.innerHTML = `${data.slip.id}`
+    })
+}
 
 getAdviceBtn.addEventListener("click",()=> getAdvice())
