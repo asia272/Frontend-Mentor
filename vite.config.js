f09f4bcdf63c21d487ee2ccf39ef0b44a8c2792b
Asia Ashraf <asiaashraf7272@gmail.com>
2025-03-31 14:27:40 +0500

    update routing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/", 
-  publicDir: "public",
   build: {
     outDir: "dist"
-  },
-  resolve: {
-    alias: {
-      "@static": resolve(__dirname, "public/static_projects") 
-    }
   }
 });
+
