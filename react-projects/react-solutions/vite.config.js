99b494339e84df3c9cc76b927ed3ec7c95e27983
Asia Ashraf <asiaashraf7272@gmail.com>
2025-04-01 09:04:52 +0500

    update configration

--- react-projects/react-solutions/vite.config.js
+++ react-projects/react-solutions/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-projects/react-solutions/", 
-  build: {
-    outDir: "dist"
-  }
+  base: '/react-projects/react-solutions/',
 });
