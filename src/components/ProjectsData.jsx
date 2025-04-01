30a7189e958a02d0a5d45639076ef14597335e26
Asia Ashraf <asiaashraf7272@gmail.com>
2025-04-01 08:50:43 +0500

    update image url

--- src/components/ProjectsData.jsx
+++ src/components/ProjectsData.jsx
@@ -1,84 +1,84 @@
 export const challenges = [
     {
         title: "Card Preview Challenge",
-        image: "/static_projects/newbie/product-preview-card-component-main/desktop-preview.jpg",
+        image: "/static-projects/newbie/product-preview-card-component-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/product-preview-card-component-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/product-preview-card-component-main",
         level: "newbie"
     },
     {
         title: "Social Link Profile",
-        image: "/static_projects/newbie/social-links-profile-main/preview.jpg",
+        image: "/static-projects/newbie/social-links-profile-main/preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/social-links-profile-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/social-links-profile-main",
         level: "newbie"
     },
     {
         title: "Blog Preview Card",
-        image: "/static_projects/newbie/blog-preview-card-main/preview.jpg",
+        image: "/static-projects/newbie/blog-preview-card-main/preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/blog-preview-card-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/blog-preview-card-main",
         level: "newbie"
     },
     {
         title: "Stats Preview Card",
-        image: "/static_projects/newbie/stats-preview-card-component-main/desktop-preview.jpg",
+        image: "/static-projects/newbie/stats-preview-card-component-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/stats-preview-card-component-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/stats-preview-card-component-main",
         level: "newbie"
     },
     {
         title: "QR Code Component",
-        image: "/static_projects/newbie/qr-code-component-main/preview.jpg",
+        image: "/static-projects/newbie/qr-code-component-main/preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/qr-code-component-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/qr-code-component-main",
         level: "newbie"
     },
     {
         title: "NFT Preview Card",
-        image: "/static_projects/newbie/nft-preview-card-component-main/desktop-preview.jpg",
+        image: "/static-projects/newbie/nft-preview-card-component-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/nft-preview-card-component-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/nft-preview-card-component-main",
         level: "newbie"
     },
     {
         title: "Profile Card",
-        image: "/static_projects/newbie/profile-card-component-main/desktop-preview.jpg",
+        image: "/static-projects/newbie/profile-card-component-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/profile-card-component-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/profile-card-component-main",
         level: "newbie"
     },
     {
         title: "3 Column Preview Card",
-        image: "/static_projects/newbie/3-column-preview-card-component-main/desktop-preview.jpg",
+        image: "/static-projects/newbie/3-column-preview-card-component-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/3-column-preview-card-component-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/3-column-preview-card-component-main",
         level: "newbie"
     },
     {
         title: "Single Price Grid",
-        image: "/static_projects/newbie/single-price-grid-component-master/desktop-preview.jpg",
+        image: "/static-projects/newbie/single-price-grid-component-master/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/single-price-grid-component-master/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/single-price-grid-component-master",
         level: "newbie"
     },
     {
         title: "Order Summary Component",
-        image: "/static_projects/newbie/order-summary-component-main/desktop-preview.jpg",
+        image: "/static-projects/newbie/order-summary-component-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/order-summary-component-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/order-summary-component-main",
         level: "newbie"
     },
     {
         title: "Recipe Page",
-        image: "/static_projects/newbie/recipe-page-main/preview.jpg",
+        image: "/static-projects/newbie/recipe-page-main/preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/newbie/recipe-page-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/newbie/recipe-page-main",
         level: "newbie"
     },
     {
         title: "Advice Generator App",
-        image: "/static_projects/junior/advice-generator-app-main/desktop-preview.jpg",
+        image: "/static-projects/junior/advice-generator-app-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/junior/advice-generator-app-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/junior/advice-generator-app-main",
         level: "junior"
@@ -92,21 +92,21 @@ export const challenges = [
     },
     {
         title: "Age Calculator App",
-        image: "/static_projects/junior/age-calculator-app-main/desktop-preview.jpg",
+        image: "/static-projects/junior/age-calculator-app-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/junior/age-calculator-app-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/junior/age-calculator-app-main",
         level: "junior"
     },
     {
         title: "CSS Grid Layout Project",
-        image: "/static_projects/junior/bento-grid-main/preview.jpg",
+        image: "/static-projects/junior/bento-grid-main/preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/junior/bento-grid-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/junior/bento-grid-main",
         level: "junior"
     },
     {
         title: "Calculator App",
-        image: "/static_projects/intermediate/calculator-app-main/desktop-preview.jpg",
+        image: "/static-projects/intermediate/calculator-app-main/desktop-preview.jpg",
         liveDemo: "https://frontend-mentor-mauve.vercel.app/static_projects/intermediate/calculator-app-main/",
         codeLink: "https://github.com/asia272/Frontend-Mentor/tree/main/public/static_projects/intermediate/calculator-app-main",
         level: "intermediate"
