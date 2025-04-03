be78c729b33752c63aae7c617552604111d18457
Asia Ashraf <asiaashraf7272@gmail.com>
2025-04-03 18:18:12 +0500

    update scrip refrence

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-solutions/',  // Correct the base URL for the deployed site
+  base: '/my-solutions/',  
 });
