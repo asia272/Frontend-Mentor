b62b5da8c8af6ee0c77dfe457b3cb42586e6d695
Asia Ashraf <asiaashraf7272@gmail.com>
2025-04-03 17:52:47 +0500

    update routing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-solutions/',  // Set this to the base path where your project is hosted on Vercel
+  base: '/my-solutions/',  // Correct the base URL for the deployed site
 });
