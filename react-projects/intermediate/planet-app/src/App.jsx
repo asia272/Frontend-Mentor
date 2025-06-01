4a396f661c116a4736dc83cc403b8655e8341f85
Asia Ashraf <asiaashraf7272@gmail.com>
2025-06-01 18:50:21 +0500

    Update browser-extension-manger build output with fixed logo paths

--- react-projects/intermediate/planet-app/src/App.jsx
+++ react-projects/intermediate/planet-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import PlanetPage from './pages/PlanetPage';
 import Navbar from './components/Navbar/Navbar';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/:planetName" element={<PlanetPage />} />
       </Routes>
     </>
---
+++ react-projects/intermediate/planet-app/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./HomePage.css";
+// import earthImg from "../assets/im"
+
+const HomePage = () => {
+  return (
+    <div className='home-page'>
+      <h1>Welcome to the Planets App</h1>
+      <p>Click on any planet in the menu to look at its information</p>
+      <img src="" alt="" />
+    </div>
+  )
+}
+
+export default HomePage
