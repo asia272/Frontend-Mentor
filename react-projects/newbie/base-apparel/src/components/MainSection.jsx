2b7337443ef685516401a5ef5dd5c03b73455960
Asia Ashraf <asiaashraf7272@gmail.com>
2025-06-03 13:14:43 +0500

    add react hook form in newbie solution

--- react-projects/newbie/base-apparel/src/components/MainSection.jsx
+++ react-projects/newbie/base-apparel/src/components/MainSection.jsx
@@ -5,29 +5,26 @@ import heroImg from "../assets/hero-desktop.jpg";
 import iconArrow from "../assets/icon-arrow.svg";
 import heroImgMobile from "../assets/hero-mobile.jpg";
 import iconError from "../assets/icon-error.svg"
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useForm } from "react-hook-form";
 
 const MainSection = () => {
-    let [email, setEmail] = useState("");
-    let [error, setError] = useState(false)
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
 
-    useEffect(() => {
-        if (email !== "" && email.includes("@")) {
-            setError(false);
-        }
-    }, [email]);
-
-    const submitMail = (e) => {
-        e.preventDefault();
-        if (email === "" || !email.includes("@")) {
-            setError(true);
-        } else {
-            setError(false);
-            alert("Email submitted successfully");
-            setEmail("")
-        }
-
+    const onSubmit = (data) => {
+        console.log(data)
+        toast.success("Email submitted successfully!");
+        reset();
     };
+
+
     return (
         <>
             <div className="logo">
@@ -43,27 +40,27 @@ const MainSection = () => {
                             Hello fellow shoppers! We're currently building our new fashion store.
                             Add your email below to stay up-to-date with announcements and our launch deals.
                         </p>
-                        <form action="#" onSubmit={submitMail}>
-                            <input type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder='Email Address' />
-                            {error &&
-                                <>
-                                    <p className='error'>Please provide a valid email</p>
-                                    <img src={iconError} alt="iconError" className='input-error' />
-                                </>
-                            }
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input
+                                type="email"
+                                placeholder="Email Address"
+                                {...register("email", {
+                                    required: "Email is required",
+                                    pattern: {
+                                        value: /^\S+@\S+$/i,
+                                        message: "Please provide a valid email",
+                                    },
+                                })}
+                            />
+                            {errors.email && <img src={iconError} alt="error-icon" className="input-error" />}
 
-
+                            {errors.email && <p className="error">{errors.email.message}</p>}
 
                             <button
                                 className='submit-btn'>
                                 <img src={iconArrow} alt="arrow-icon" />
 
                             </button>
-
-
                         </form>
                     </div>
                 </div>
